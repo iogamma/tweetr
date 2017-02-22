f1e52dd84f1f4e3fbf708bde0ee58ea184acef29
Hal Tang <hal.wh.tang@gmail.com>
2017-02-22 16:56:23 +0000

    Implemented tweets rendering capability

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,53 +4,88 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: {
-      small:   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-      regular: "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-      large:   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+// Fake data taken from tweets.json
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
     },
-    handle: "@SirIsaac"
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "<strong>This is a test</strong> $(.posted-tweets)"
+    },
+    "created_at": 1461113959088
   },
-  created_at: 1461116232227
-}
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
 
 $(document).ready(function() {
 
-  function createTweetElement(tweetData) {
+  function createTweetElement(tweet) {
     const $newTweet = $('<article>').addClass('tweet');
 
     $newTweet.append(
       `<header>
-        <img class="avatar" src="${tweetData.user.avatars.regular}">
-        <h1 class="name">${tweetData.user.name}</h1>
-        <span class="handle">${tweetData.user.handle}</span>
+        <img class="avatar" src="${tweet.user.avatars.regular}">
+        <h1 class="name">${tweet.user.name}</h1>
+        <span class="handle">${tweet.user.handle}</span>
       </header>
       <p>
-        ${tweetData.content.text}
+        ${tweet.content.text}
       </p>
       <footer>
-        <span class="date_created">${tweetData.created_at} days ago</span>
+        <span class="date_created">${tweet.created_at} days ago</span>
         <i class="fa fa-heart"></i>
         <i class="fa fa-retweet"></i>
         <i class="fa fa-flag"></i>
       </footer>`);
-    console.log($newTweet);
+
     return $newTweet;
   }
 
   function renderTweets(tweetsArr) {
+    const $postedTweets = $('.posted-tweets');
 
+    for (let tweet of tweetsArr) {
+      $postedTweets.append(createTweetElement(tweet));
+    }
   }
 
   //========== TEST CODE
-  let $tweet = createTweetElement(tweetData);
-  console.log($tweet);
-  $('.posted-tweets').append($tweet);
+  console.log(data);
+  renderTweets(data);
 
 });
